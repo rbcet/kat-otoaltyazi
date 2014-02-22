e2bfd3c97d16664e4c3df68e38a669a7228aaa4e
katates <lroyalchildl@gmail.com>
2014-02-22 22:33:11 +0200

    Updated

--- files/takip.cmd
+++ files/takip.cmd
@@ -31,16 +31,25 @@ GOTO ERR1
 
 
 :ScheduleMatch
-
+	set currentParameter=%1
 	set /p match=<%watchsettings%
 	call set match=%%match:PATH_HERE=%var1%%%
 	call set match=%match:~0,-1%
 
-	powershell %match%; exit
+	powershell %match%; 
+	
+   
+if exist "%currentParameter:~1,-1%\*.srt" (
+msg * test222
+) else (
+msg * test
+)
+
+	exit
 
 	if not errorlevel 0 GOTO ERR1
 
-GOTO FINISH
+GOTO kontrol
 
 
 :ERR1
