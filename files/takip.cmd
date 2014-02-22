87b5bb14bb1690879feb1dee54f80de27576cba2
katates <lroyalchildl@gmail.com>
2014-02-22 21:55:17 +0200

    Uploaded necessary files

---
+++ files/takip.cmd
@@ -0,0 +1,53 @@
+@echo OFF
+
+:SETUP
+	set watchsettings="C:\Program Files\FileBot\OtoAltyazi\takip_ayari.txt"
+GOTO DetermineJobType
+
+:DetermineJobType
+	:: determine if adding or removing a task
+
+	set var1=%1
+	set var1=%var1:"=%
+
+	set var2=%2
+	set var2=%var2:"=%
+
+	set var3=%var1:\=-%
+	set var3=%var3::=%
+
+	if "%var2%"=="setnonmatch" (
+		goto ScheduleNonMatch
+	) ELSE ( 
+		if "%var2%"=="setmatch" (
+			goto ScheduleMatch
+		) ELSE (
+			GOTO ERR1
+			)
+		)
+	)
+
+GOTO ERR1
+
+
+:ScheduleMatch
+
+	set /p match=<%watchsettings%
+	call set match=%%match:PATH_HERE=%var1%%%
+	call set match=%match:~0,-1%
+
+	powershell %match%; exit
+
+	if not errorlevel 0 GOTO ERR1
+
+GOTO FINISH
+
+
+:ERR1
+	echo **** Warning: Something Didn't Work. Please Confirm Settings ****
+	pause>nul
+GOTO FINISH
+
+
+:FINISH
+EXIT /B
