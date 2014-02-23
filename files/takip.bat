93c4e9c2a683fae54c221c7db04bbcd27a65d382
katates <lroyalchildl@gmail.com>
2014-02-23 21:50:33 +0200

    Uploaded new lines

---
+++ files/takip.bat
@@ -0,0 +1,72 @@
+@echo OFF
+:SETUP
+	set watchsettings="C:\Program Files\FileBot\OtoAltyazi\takip_ayari.txt"
+GOTO DetermineJobType
+
+:DetermineJobType
+	:: determine if adding or removing a task
+
+	set var1=%1
+	set var1=%var1:"=%
+
+	set var2=%2
+	set var2=%var2:"=%
+
+	set var3=%var1:\=-%
+	set var3=%var3::=%
+
+	if "%var2%"=="setnonmatch" (
+		goto ScheduleNonMatch
+	) ELSE ( 
+		if "%var2%"=="setmatch" (
+			goto ScheduleMatch
+		) ELSE (
+			GOTO ERR1
+			)
+		)
+	)
+
+GOTO ERR1
+
+:ScheduleMatch
+
+echo. 2> %1\yeni.txt 
+dir /b /s "%1" | findstr /m /i "\.srt$" > %1\eski.txt 
+
+	set currentParameter=%1	
+	set /p match=<%watchsettings%
+	call set match=%%match:PATH_HERE=%var1%%%
+	call set match=%match:~0,-1%
+	powershell %match%; 
+	
+dir /b /s "%1" | findstr /m /i "\.srt$"  > %1\yeni.txt 
+fc /b %1\eski.txt %1\yeni.txt|find /i "no differences">nul 
+if errorlevel 1 goto farkli  
+if not errorlevel 1 goto olustur 
+
+:farkli 
+msg * %1 e yeni bir altyazi eklendi! 
+goto olustur 
+
+:olustur 
+dir /b /s "%1" | findstr /m /i "\.srt$"  > %1\eski.txt 
+goto cik 
+
+:cik 
+ATTRIB +H %1/eski.txt
+ATTRIB +H %1/yeni.txt
+exit 
+
+	if not errorlevel 0 GOTO ERR1
+
+GOTO FINISH
+
+
+:ERR1
+	echo **** Warning: Something Didn't Work. Please Confirm Settings ****
+	pause>nul
+GOTO FINISH
+
+
+:FINISH
+EXIT /B
