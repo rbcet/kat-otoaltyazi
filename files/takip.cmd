d1ca18d35643177a58a1d3d17a615f7a4f20daea
katates <lroyalchildl@gmail.com>
2014-02-28 00:58:02 +0200

    Updated!

--- files/takip.cmd
+++ files/takip.cmd
@@ -35,13 +35,12 @@ GOTO ERR1
 
 :ScheduleMatch
 	set currentParameter=%1	
-	set he=Evet, klasore git -
-	set yok= Yes, open folder
-	set eklendibaslik=EKLENEN ALTYAZILAR - ADDED SUBTITLES
+	set he=Eklenen Altyazilar -
+	set yok= Added Subtitles;
+	set eklendibaslik=Klasore git? - Go to folder?
 ATTRIB -H %1\eski.txt 
 ATTRIB -H %1\yeni.txt
-ATTRIB -H %1\eklendi.txt
-
+	
 echo Option Explicit>> %1\eklendi.vbs
 echo Const conForReading = ^1>> %1\eklendi.vbs
 echo Dim objFSO, objReadFile, objFile, contents, result, shell>> %1\eklendi.vbs
@@ -51,7 +50,7 @@ echo. >> %1\eklendi.vbs
 echo If objFile.Size > 0 Then >> %1\eklendi.vbs
 echo Set objReadFile = objFSO.OpenTextFile("%1\eklendi.txt", 1, False)>> %1\eklendi.vbs
 echo contents = objReadFile.ReadAll>> %1\eklendi.vbs
-echo result = MsgBox ("" ^& contents ^& "%he% %yok%",vbYesNo+vbExclamation,"%eklendibaslik%")>> %1\eklendi.vbs
+echo result = MsgBox ("%he% %yok%" ^& vbCr ^& contents ^& "",vbYesNo+vbExclamation,"%eklendibaslik%")>> %1\eklendi.vbs
 echo Select Case result>> %1\eklendi.vbs
 echo Case vbYes>>  %1\eklendi.vbs
 echo Set shell = wscript.CreateObject("Shell.Application")>> %1\eklendi.vbs
@@ -67,7 +66,7 @@ echo Set objFSO = Nothing >> %1\eklendi.vbs
 echo Set objReadFile = Nothing >> %1\eklendi.vbs
 echo WScript.Quit() >> %1\eklendi.vbs
 
-	echo. 2> %1\yeni.txt 
+ 	echo. 2> %1\yeni.txt 
 	dir /b /s "%1" | findstr /m /i "\.srt$" > %1\eski.txt 
 	set filemask="%1\eski.txt"
 	for %%A in (%filemask%) do if %%~zA==0 echo . >> %1\eski.txt
@@ -86,7 +85,9 @@ if not errorlevel 1 goto olustur
 :farkli 
 set dosya1="%1\eski.txt" 
 set dosya2="%1\yeni.txt" 
-findstr /G:%dosya1% /I /L /B /V %dosya2% > %1\eklendi.txt 
+setlocal enabledelayedexpansion    
+for /f "delims=" %%a in ('findstr /G:%dosya1% /I /L /B /V %dosya2%') do (@echo %%~nxa >> %1\eklendi.txt )  
+endlocal  
 wscript %1\eklendi.vbs
 
 goto olustur 
@@ -95,6 +96,8 @@ goto olustur
 dir /b /s "%1" | findstr /m /i "\.srt$"  > %1\eski.txt 
 ATTRIB +H %1\eski.txt 
 ATTRIB +H %1\yeni.txt
+ATTRIB -R -S -H %1\eklendi.txt
+CD.>%1\eklendi.txt  
 ATTRIB +H %1\eklendi.txt
 del "%1\eklendi.vbs"
 exit
