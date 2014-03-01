8e7aab25d9d85d54bf296bdd12442051563ee3f5
katates <lroyalchildl@gmail.com>
2014-03-01 17:20:45 +0200

    Updated!

--- files/otoaltyazi.cmd
+++ files/otoaltyazi.cmd
@@ -309,7 +309,8 @@ echo setlocal enabledelayedexpansion    >> kontrol.bat
 echo for /f "delims=" %yuzde%%yuzde%a in ('findstr /G:%%dosya1%% /I /L /B /V %%dosya2%%') do (@echo %yuzde%%yuzde%~nxa ^>^> C:\Progra~1\FileBot\OtoAltyazi\eklendi.txt )  >> kontrol.bat
 echo endlocal  >> kontrol.bat
 echo. >> kontrol.bat 
-echo wscript //T:300 C:\Progra~1\FileBot\OtoAltyazi\eklendi.vbs >> kontrol.bat
+echo start "" wscript "C:\Progra~1\FileBot\OtoAltyazi\eklendi.vbs" >> kontrol.bat
+echo ping 192.0.2.2 -n 1 -w 2500 ^> nul >> kontrol.bat
 echo ATTRIB -R -S -H "C:\Progra~1\FileBot\OtoAltyazi\eklendi.txt" >> kontrol.bat
 echo CD.^>C:\Progra~1\FileBot\OtoAltyazi\eklendi.txt  >> kontrol.bat
 echo goto olustur >> kontrol.bat
